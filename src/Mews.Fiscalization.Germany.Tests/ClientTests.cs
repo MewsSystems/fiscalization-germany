8f70dac18286dd9cc893e9bd1c3bc61678ba1eb0
abdallahbeshi <abdallah.altrabeishi@mews.com>
2020-09-15 17:07:04 +0200

    self

--- src/Mews.Fiscalization.Germany.Tests/ClientTests.cs
+++ src/Mews.Fiscalization.Germany.Tests/ClientTests.cs
@@ -8,10 +8,10 @@ namespace Mews.Fiscalization.Germany.Tests
     [TestFixture]
     public class ClientTests
     {
-        private static readonly Guid ClientId = new Guid("f84da730-8074-4d40-8e7b-65998a7cec25");
-        private static readonly Guid TssId = new Guid("f84da730-8074-4d40-8e7b-65998a7cec25");
-        private static readonly ApiKey ApiKey = new ApiKey("test_dgyieu2fhj1jfj5em2pak6dli_test");
-        private static readonly ApiSecret ApiSecret = new ApiSecret("k3t6OZFfBtXaIfZsNPTWFn7KAFZQ3YAIjmLKZePTw16");
+        private static readonly Guid ClientId = new Guid("INSERT_CLIENT_ID");
+        private static readonly Guid TssId = new Guid("INSERT_TSS_ID");
+        private static readonly ApiKey ApiKey = new ApiKey("INSERT_API_KEY");
+        private static readonly ApiSecret ApiSecret = new ApiSecret("INSERT_API_Secret");
 
         [Test]
         public async Task CreateClientSucceeds()
