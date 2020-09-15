ecdb4e593757ebb4e49d8dfaa2462df29629475e
abdallahbeshi <abdallah.altrabeishi@mews.com>
2020-09-15 14:46:54 +0200

    self

--- src/Mews.Fiscalization.Germany.Tests/TransactionTests.cs
+++ src/Mews.Fiscalization.Germany.Tests/TransactionTests.cs
@@ -10,10 +10,10 @@ namespace Mews.Fiscalization.German.Tests
     [TestFixture]
     public class Tests
     {
-        private static readonly Guid ClientId = new Guid("f84da730-8074-4d40-8e7b-65998a7cec25");
-        private static readonly Guid TssId = new Guid("f84da730-8074-4d40-8e7b-65998a7cec25");
-        private static readonly ApiKey ApiKey = new ApiKey("test_dgyieu2fhj1jfj5em2pak6dli_test");
-        private static readonly ApiSecret ApiSecret = new ApiSecret("k3t6OZFfBtXaIfZsNPTWFn7KAFZQ3YAIjmLKZePTw16");
+        private static readonly Guid ClientId = new Guid("INSERT_CLIENT_ID");
+        private static readonly Guid TssId = new Guid("INSERT_TSS_ID");
+        private static readonly ApiKey ApiKey = new ApiKey("INSERT_API_KEY");
+        private static readonly ApiSecret ApiSecret = new ApiSecret("INSERT_API_Secret");
 
         [Test]
         public async Task StatusCheckSucceeds()
@@ -26,11 +26,14 @@ namespace Mews.Fiscalization.German.Tests
         }
 
         [Test]
-        public async Task CreateClient()
+        public async Task CreateClientSucceeds()
         {
             var client = GetClient();
             var accessToken = (await client.GetAccessTokenAsync()).SuccessResult;
             var createdClient = await client.CreateClientAsync(accessToken, TssId);
+
+            Assert.IsTrue(createdClient.IsSuccess);
+            Assert.IsNotNull(createdClient.SuccessResult.Id);
         }
 
         [Test]
