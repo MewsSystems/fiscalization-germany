6955389e63991665441bed5927b8436febfffc4d
abdallahbeshi <abdallah.altrabeishi@mews.com>
2020-10-21 19:16:45 +0200

    self

--- src/Mews.Fiscalization.Germany.Tests/TestFixture.cs
+++ src/Mews.Fiscalization.Germany.Tests/TestFixture.cs
@@ -5,10 +5,18 @@ namespace Mews.Fiscalization.Germany.Tests
 {
     public static class TestFixture
     {
-        private static readonly Guid ClientId = new Guid(Environment.GetEnvironmentVariable("client_Id") ?? "INSERT_CLIENT_ID");
-        private static readonly Guid TssId = new Guid(Environment.GetEnvironmentVariable("tss_id") ?? "INSERT_TSS_ID");
-        private static readonly ApiKey ApiKey = new ApiKey(Environment.GetEnvironmentVariable("api_key") ?? "INSERT_API_KEY");
-        private static readonly ApiSecret ApiSecret = new ApiSecret(Environment.GetEnvironmentVariable("api_secret") ?? "INSERT_API_SECRET");
+        private static readonly Guid ClientId;
+        private static readonly Guid TssId;
+        private static readonly ApiKey ApiKey;
+        private static readonly ApiSecret ApiSecret;
+
+        static TestFixture()
+        {
+            ClientId = new Guid(Environment.GetEnvironmentVariable("client_Id") ?? "INSERT_CLIENT_ID");
+            TssId = new Guid(Environment.GetEnvironmentVariable("tss_id") ?? "INSERT_TSS_ID");
+            ApiKey = new ApiKey(Environment.GetEnvironmentVariable("api_key") ?? "INSERT_API_KEY");
+            ApiSecret = new ApiSecret(Environment.GetEnvironmentVariable("api_secret") ?? "INSERT_API_SECRET");
+        }
 
         public static ClientData GetClientData()
         {
