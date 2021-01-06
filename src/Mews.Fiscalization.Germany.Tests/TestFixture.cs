fe0d6a703305431a66b9179144a0445600d9ae65
abdallahbeshi <abdallah.altrabeishi@mews.com>
2021-01-06 21:27:42 +0100

    Updated to latest version of core library.

--- src/Mews.Fiscalization.Germany.Tests/TestFixture.cs
+++ src/Mews.Fiscalization.Germany.Tests/TestFixture.cs
@@ -7,8 +7,8 @@ namespace Mews.Fiscalization.Germany.Tests
     {
         public static readonly Guid ClientId = new Guid(Environment.GetEnvironmentVariable("client_Id") ?? "INSERT_CLIENT_ID");
         public static readonly Guid TssId = new Guid(Environment.GetEnvironmentVariable("tss_id") ?? "INSERT_TSS_ID");
-        public static readonly ApiKey ApiKey = new ApiKey(Environment.GetEnvironmentVariable("api_key") ?? "INSERT_API_KEY");
-        public static readonly ApiSecret ApiSecret = new ApiSecret(Environment.GetEnvironmentVariable("api_secret") ?? "INSERT_API_SECRET");
+        public static readonly ApiKey ApiKey = ApiKey.Create(Environment.GetEnvironmentVariable("api_key") ?? "INSERT_API_KEY").Success.Get();
+        public static readonly ApiSecret ApiSecret = ApiSecret.Create(Environment.GetEnvironmentVariable("api_secret") ?? "INSERT_API_SECRET").Success.Get();
 
         public static FiskalyClient GetFiskalyClient()
         {
--- src/Mews.Fiscalization.Germany/Model/Types/ApiKey.cs
+++ src/Mews.Fiscalization.Germany/Model/Types/ApiKey.cs
@@ -1,19 +1,25 @@
-﻿using Mews.Fiscalization.Core.Model;
+﻿using FuncSharp;
+using Mews.Fiscalization.Core.Model;
+using System.Text.RegularExpressions;
 
 namespace Mews.Fiscalization.Germany.Model
 {
-    public sealed class ApiKey : LimitedString
+    public sealed class ApiKey
     {
-        private static readonly StringLimitation Limitation = new StringLimitation(maxLength: 512, pattern: ".*[^\\s].*", allowEmptyOrWhiteSpace: false);
-
-        public ApiKey(string value)
-            : base(value, Limitation)
+        private ApiKey(string value)
         {
+            Value = value;
         }
 
-        public static bool IsValid(string value)
+        public string Value { get; }
+
+        public static ITry<ApiKey, INonEmptyEnumerable<Error>> Create(string value)
         {
-            return IsValid(value, Limitation);
+            return StringValidations.LengthInRange(value, 1, 512).FlatMap(v =>
+            {
+                var validApiKey = StringValidations.RegexMatch(v, new Regex(".*[^\\s].*"));
+                return validApiKey.Map(k => new ApiKey(k));
+            });
         }
     }
 }
